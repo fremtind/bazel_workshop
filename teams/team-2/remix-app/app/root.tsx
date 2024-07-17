4fe42b687a6c5f1a8cc862a4272d845c6f3ff430
Knut Eirik Leira Hjelle <knut.hjelle@fremtind.no>
2024-07-17 13:38:36 +0200

    Added frontend lib example

--- teams/team-2/remix-app/app/root.tsx
+++ teams/team-2/remix-app/app/root.tsx
@@ -6,6 +6,7 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 import "./tailwind.css";
+import { Greeting } from "greeting-component/src/greeting";
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
@@ -17,6 +18,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Links />
       </head>
       <body>
+        <Greeting name="Remix" />
         {children}
         <ScrollRestoration />
         <Scripts />
